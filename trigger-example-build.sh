464ef3ab99c59cb5aaffa76bd84c40ad6b5e12cf
Pete Romano <pete.romano@yahoo.com>
2013-04-19 18:11:54 -0400

    Updating the trigger script

--- trigger-example-build.sh
+++ trigger-example-build.sh
@@ -3,4 +3,7 @@
 date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s" >> TRIGGER_EXAMPLE
 git add TRIGGER_EXAMPLE
 git commit -m"Triggering example build..."
+git checkout staging-example
+git merge devel-example
 git push
+git checkout devel-example
